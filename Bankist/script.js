6c2b4932820c5eb1e928f8e5ecf652e95efcd5ad
Umesh Siddarth <umeshsiddarth@gmail.com>
2023-08-30 18:09:15 +0530

    Login implementation

--- Bankist/script.js
+++ Bankist/script.js
@@ -58,23 +58,165 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 // Transaction history
-const displayTransactions = function (transactions) {
+const displayTransactions = function (account) {
   containerMovements.innerHTML = "";
-  transactions.forEach(function (trans, i) {
+  account.transactions.forEach(function (trans, i) {
     const type = trans > 0 ? "deposit" : "withdrawal";
 
-    const html = `
+    const html =
+      type === "withdrawal"
+        ? `
         <div class="movements__row">
             <div class = "movements__type movements__type--${type}">
             ${i + 1} ${type} </div>
-            <div class = "movements__value">${trans}</div>
+            <div class = "movements__value--${type}">INR ${Math.abs(
+            trans
+          )}</div>
         </div> 
+        `
+        : `
+        <div class="movements__row">
+            <div class = "movements__type movements__type--interest">
+            ${i + 1} INTEREST </div>
+            <div class = "movements__value--${type}">INR ${
+            (trans * account.interestRate) / 100
+          }</div>
+        </div> 
+        <div class="movements__row">
+            <div class = "movements__type movements__type--${type}">
+            ${i + 1} ${type} </div>
+            <div class = "movements__value--${type}">INR ${Math.abs(
+            trans
+          )}</div>
+        </div> 
+        
         `;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
 
-displayTransactions(account1.transactions);
+const displaySummary = function (account) {
+  const income = account.transactions
+    .filter((trans) => trans > 0)
+    .reduce((acc, cur) => (acc += cur), 0);
+
+  const outgoing = Math.abs(
+    account.transactions
+      .filter((trans) => trans < 0)
+      .reduce((acc, cur) => (acc += cur), 0)
+  );
+  // This bank provides interest of 3% for every deposit.
+  const interest = account.transactions
+    .filter((trans) => trans > 0)
+    .filter((dep) => dep >= 1)
+    .map((dep) => (dep * account.interestRate) / 100)
+    .reduce((acc, cur) => (acc += cur), 0);
+
+  account.balance =
+    account.transactions.reduce((acc, cur) => acc + cur, 0) + interest;
+
+  labelSumIn.textContent = `INR ${income}`;
+  labelSumOut.textContent = `INR ${outgoing}`;
+  labelSumInterest.textContent = `INR ${interest}`;
+  labelBalance.textContent = `INR ${account.balance}`;
+};
 
 // Computing Usernames
+// Test
+// const user = "Umesh Siddarth";
+// const username = user
+//   .toLowerCase()
+//   .split(" ")
+//   .map(function (el) {
+//     // return el.slice(0, 1);
+//     return el[0];
+//   })
+//   .join("");
+// console.log(username);
+
+// No need to return anything. We are just creating side effects with forEach
+const createUserName = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+createUserName(accounts);
+// console.log(accounts);
+
+// Calculate the balance
+
+// const calcBalance = function (account) {
+//   const balance = account.transactions.reduce((acc, cur) => acc + cur, 0);
+//   return balance + Number(labelSumInterest.textContent);
+// };
+
+// calcBalance(account2.transactions);
+// console.log(accounts);
+
+function updateUI(account) {
+  // Display transactions
+  displayTransactions(account);
+
+  // Display Summary and balance
+  displaySummary(account);
+}
+
+// Implementing Login Functionality
+let activeAccount;
+
+btnLogin.addEventListener("click", function (e) {
+  e.preventDefault();
+  activeAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+
+  // Display Welcome message
+  if (activeAccount?.pin === Number(inputLoginPin.value)) {
+    labelWelcome.textContent = `Welcome back, ${
+      activeAccount.owner.split(" ")[0]
+    }`;
+
+    // Clear Fields
+    // inputLoginUsername.value = "";
+    // inputLoginPin.value = "";
+    // // The above line is equivalent to
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur(); // This will remove the focus on the pin box.
+
+    //  Display account details
+    containerApp.style.opacity = 100;
+
+    updateUI(activeAccount);
+  } else {
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = `Try Again`;
+  }
+});
+
+// Implementing Transfers
+btnTransfer.addEventListener("click", (e) => {
+  e.preventDefault();
+  // console.log("Click");
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+  const amount = Number(inputTransferAmount.value);
+  inputTransferAmount.value = inputTransferTo.value = "";
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    amount <= activeAccount.balance &&
+    receiverAcc.username !== activeAccount.username
+  ) {
+    activeAccount.transactions.push(-amount);
+    receiverAcc.transactions.push(amount);
+    updateUI(activeAccount);
+  } else {
+    console.log(`Invalid Attempt`);
+  }
+});
