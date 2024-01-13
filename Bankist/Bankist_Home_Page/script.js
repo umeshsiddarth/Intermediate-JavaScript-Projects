6614b91066bf082106e0cb427d6214e49892eea5
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-01-13 10:17:00 +0530

    Added Nav bar and tabbed components

--- Bankist/Bankist_Home_Page/script.js
+++ Bankist/Bankist_Home_Page/script.js
@@ -34,6 +34,8 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+// -----------------------------------------------------------------------------------
+
 // Smooth Scrolling
 // Old School way
 const buttonScrollTo = document.querySelector(".btn--scroll-to");
@@ -52,4 +54,60 @@ const section1 = document.querySelector("#section--1");
 // New Way
 buttonScrollTo.addEventListener("click", (e) => {
   section1.scrollIntoView({ behavior: "smooth" });
+});
+
+// --------------------------------------------------------------------------------------------
+
+// Page Navigation
+
+// This method is not optimized.
+
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href"); // Using getAttribute instead of direct href is because we don't need the full URL
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// What if we have 100s of element? We cannot create 100 functions to do the same job. So we use Event delegation.
+// Here we added one bif event Handler to the parent element and determine the target element and do what is required.
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  // Matching Strategy
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// -----------------------------------------------------------------------------------------
+
+// Tabbed Component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab"); // Here when we click the numbers 01 or 02 the span element was clicked, but we need the buttons to be clicked. Simple e.target.parent won't work here as when we click button it select's button's parent element. So we use e.target.closest('') to get the button element always.
+
+  // When we click the tab area it returns null.
+  // Guard Clause
+  if (!clicked) return;
+
+  // Active Tab
+  tabs.forEach((btn) => {
+    btn.classList.remove("operations__tab--active");
+  });
+  clicked.classList.add("operations__tab--active");
+
+  // Active Content Area
+  // Here we make use of Dataset which is data-tab attribute or attributes with data- prefix
+  tabsContent.forEach((tabContent) => {
+    tabContent.classList.remove("operations__content--active");
+  });
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
